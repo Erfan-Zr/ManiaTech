c8d07037444cf76a1d918ec924f7cc2b3fe8bd42
Erfan-Zr <lockelerz@gmail.com>
2023-11-09 22:45:17 +0330

    finished sidebar and fixed some other issues in ui still wip

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <Sidebar />
         {children}
       </body>
     </html>
--- app/layouts/header.jsx
+++ app/layouts/header.jsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import React from "react";
 import logo from "@/public/logo.png";
 import { MegaMenu, Searchbar, ShoppingCart } from "../(components)";
+import { Sidebar } from ".";
+import profile from "@/public/icons/profile.svg";
 
 const Header = () => {
   return (
     <div className="flex-col sticky top-0 justify-center items-center">
       <div className="flex-col bg-slate-400 justify-center items-center">
-        <div className="grid grid-cols-4 items-center  py-2  px-8 w-[85%]">
+        <div className="grid grid-cols-5 items-center py-2 px-8 max-md:justify-items-center">
+          <Sidebar />
           <Image
             src={logo}
             alt="ManiaTech"
@@ -15,13 +18,20 @@ const Header = () => {
             height={50}
             priority={true}
           />
+          <Image
+            src={profile}
+            alt="profile icon"
+            width={30}
+            height={30}
+            priority={true}
+            className=""
+          />
           <Searchbar />
-          <p>Customer Support </p>
+          <p className="max-md:hidden">Customer Support </p>
           <ShoppingCart />
           {/* <Image src="" /> */}
         </div>
       </div>
-
       <MegaMenu />
     </div>
   );
